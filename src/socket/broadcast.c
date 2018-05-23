2f9d6de54651c94fa49e52ad5ea158c138980e89
Ryuichi <you@example.com>
2018-05-23 22:21:35 +0900

    ブロードキャスト

---
+++ src/socket/broadcast.c
@@ -0,0 +1,110 @@
+#include "socket.h"
+
+struct broadcast_info {
+    unsigned short port;     /* ポート番号 */
+
+    int sd;                  /* ソケットディスクリプタ */
+    struct sockaddr_in addr; /* ブロードキャストのアドレス構造体 */
+};
+typedef struct broadcast_info bc_info_t;
+
+#define MAXRECVSTRING 255  /* Longest string to receive */
+
+static int broadcast_receive(bc_info_t *info, char *errmsg) {
+    char recv_msg[MAXRECVSTRING+1];
+    int recv_msglen = 0;
+
+    /* Receive a single datagram from the server */
+    recv_msglen = recvfrom(info->sd, recv_msg, MAXRECVSTRING, 0, NULL, 0);
+    if(recv_msglen < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
+    }
+
+    recv_msg[recv_msglen] = '\0';
+    printf("Received: %s\n", recv_msg);    /* Print the received string */
+
+    return(0);
+}
+ 
+static int socket_initialize(bc_info_t *info, char *errmsg) {
+    int rc = 0;
+
+    /* ソケットの生成 : UDPを指定する */
+    info->sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(info->sd < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
+    }
+
+    /* ブロードキャストのアドレス構造体を作成する */
+    info->addr.sin_family = AF_INET;
+    info->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    info->addr.sin_port = htons(info->port);
+
+    /* ブロードキャストポートにバインドする*/
+    rc = bind(info->sd, (struct sockaddr *)&(info->addr),
+              sizeof(info->addr));
+    if(info->sd < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
+    }
+
+    return(0);
+}
+
+static void socket_finalize(bc_info_t *info) {
+    /* ソケット破棄 */
+    if(info->sd != 0) close(info->sd);
+
+    return;
+}
+
+static int broadcast_receiver(bc_info_t *info, char *errmsg) {
+    int rc = 0;
+
+    /* ソケットの初期化 */
+    rc = socket_initialize(info, errmsg);
+    if(rc != 0) return(-1);
+
+    /* ブロードキャストを送信する */
+    rc = broadcast_receive(info, errmsg);
+
+    /* ソケットの終期化 */
+    socket_finalize(info);
+
+    return(0);
+}
+
+static int initialize(int argc, char *argv[], bc_info_t *info, char *errmsg) {
+    if(argc != 2){
+        sprintf(errmsg, "Usage: %s <port>", argv[0]);
+        return(-1);
+    }
+
+    memset(info, 0, sizeof(bc_info_t));
+    info->port       = atoi(argv[1]);
+
+    return(0);
+}
+
+int main(int argc, char *argv[]) {
+    int rc = 0;
+    bc_info_t info = {0};
+    char errmsg[BUFSIZ];
+
+    rc = initialize(argc, argv, &info, errmsg);
+    if(rc != 0){ 
+        fprintf(stderr, "Error: %s\n", errmsg);
+        return(-1);
+    }   
+
+    rc = broadcast_receiver(&info, errmsg);
+    if(rc != 0){ 
+        fprintf(stderr, "Error: %s\n", errmsg);
+        return(-1);
+    }
+
+    return(0);
+}
+
