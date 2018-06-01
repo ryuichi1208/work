06c901615ae1040f6d49d48019c2baf40394cc54
Ryuichi <ryuichi1208>
2018-06-01 18:54:31 +0900

    コメント修正

--- src/socket/socket.h
+++ src/socket/socket.h
@@ -4,7 +4,9 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <unistd.h> 
+#include <errno.h>
 
 #define QUEUELIMIT 5
 #define MSGSIZE 1024
 #define BUFSIZE (MSGSIZE + 1)
+#define MAXRECVSTRING 255
